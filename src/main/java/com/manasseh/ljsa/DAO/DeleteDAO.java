f82f51fe0486cfdf0d88282c644ea1cedf7b079a
Manasseh Randriamitsiry <70688966+manasseh-randriamitsiry@users.noreply.github.com>
2022-10-25 15:12:22 +0300

    total and average added

--- src/main/java/com/manasseh/ljsa/DAO/DeleteDAO.java
+++ src/main/java/com/manasseh/ljsa/DAO/DeleteDAO.java
@@ -1,19 +1,19 @@
 package com.manasseh.ljsa.DAO;
 
+import com.manasseh.ljsa.utils.DatabaseConnection;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import com.manasseh.ljsa.utils.PopUp;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Optional;
 
 public class DeleteDAO {
     public void delete(Integer id,String tableName,String idName) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/ljsa","root","123456789");
+        DatabaseConnection conn = new DatabaseConnection();
+        Connection connection = conn.getConnection();
         String sql = "DELETE FROM "+tableName+" WHERE "+idName+"="+id;
         PreparedStatement statement = connection.prepareStatement(sql);
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
