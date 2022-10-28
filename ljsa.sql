d598c45f60aa44e861c3b1344bd18154f04cf21b
Manasseh Randriamitsiry <70688966+manasseh-randriamitsiry@users.noreply.github.com>
2022-10-28 23:38:17 +0300

    sql added

--- ljsa.sql
+++ ljsa.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Oct 28, 2022 at 08:31 PM
--- Server version: 8.0.18
--- PHP Version: 8.1.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `ljsa`
 --
+
+CREATE DATABASE IF NOT EXISTS `ljsa` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `ljsa`;
 
 -- --------------------------------------------------------
 
